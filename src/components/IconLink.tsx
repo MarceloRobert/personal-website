11f67c3e2c6dca378f53620bed889720bd0db8bc
Marcelo Robert Santos <4mrSantos@gmail.com>
2026-09-11 14:04:27 -0300

    feat: add link icons

---
+++ src/components/IconLink.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+export const IconLink = ({
+    text,
+    href,
+    iconSrc,
+    iconAlt,
+    iconWidth = 32,
+    iconHeight = 32,
+    iconClassName,
+}: {
+    text: string;
+    href: string;
+    iconSrc: string;
+    iconAlt: string;
+    iconWidth?: number;
+    iconHeight?: number;
+    iconClassName?: string;
+}) => {
+    return <div className="flex items-center justify-center gap-2">
+        <Image
+            className={twMerge("dark:invert size-8", iconClassName)}
+            src={iconSrc}
+            alt={iconAlt}
+            width={iconWidth}
+            height={iconHeight}
+            priority />
+        <Link href={href} target="_blank" className="text-lg">
+            {text}
+        </Link>
+    </div>;
+}
